4d799b9b63ecb50bc438108acf4ba3611e7589f9
Luke Memet <lukememet@gmail.com>
2022-08-30 22:36:01 -0400

    New approach to getting inverse of UIColor to avoid crash
    Fix

--- MKColorPicker/Classes/UIColor+Extension.swift
+++ MKColorPicker/Classes/UIColor+Extension.swift
@@ -10,58 +10,12 @@
 
 import UIKit
 
+/// https://stackoverflow.com/questions/5893261/how-to-get-inverse-color-from-uicolor
 
-/// https://gist.github.com/gotev/76df9006674762859626846cf171ff80
 extension UIColor {
     
-    open var redValue: CGFloat{
-        return cgColor.components! [0]
-    }
-    
-    open var greenValue: CGFloat{
-        return cgColor.components! [1]
-    }
-    
-    open var blueValue: CGFloat{
-        return cgColor.components! [2]
-    }
-    
-    open var alphaValue: CGFloat{
-        return cgColor.components! [3]
-    }
-    
-    var isWhiteText: Bool {
-        
-        let red = self.redValue * 255
-        let green = self.greenValue * 255
-        let blue = self.blueValue * 255
-        
-        // https://en.wikipedia.org/wiki/YIQ
-        // https://24ways.org/2010/calculating-color-contrast/
-        let yiq = ((red * 299) + (green * 587) + (blue * 114)) / 1000
-        return yiq < 192
-    }
-    
     open var inverted: UIColor {
-        return UIColor(red: (1.0 - self.redValue), green: (1.0 - greenValue), blue: (1.0 - self.blueValue), alpha: self.alphaValue) // Assuming you want the same alpha value.
-    }
-    
-    public convenience init(hex: String) {
-        let scanner = Scanner(string: hex)
-        scanner.scanLocation = 0
-        
-        var rgbValue: UInt64 = 0
-        
-        scanner.scanHexInt64(&rgbValue)
-        
-        let r = (rgbValue & 0xff0000) >> 16
-        let g = (rgbValue & 0xff00) >> 8
-        let b = rgbValue & 0xff
-        
-        self.init(
-            red: CGFloat(r) / 0xff,
-            green: CGFloat(g) / 0xff,
-            blue: CGFloat(b) / 0xff, alpha: 1
-        )
+        var a: CGFloat = 0.0, r: CGFloat = 0.0, g: CGFloat = 0.0, b: CGFloat = 0.0
+        return getRed(&r, green: &g, blue: &b, alpha: &a) ? UIColor(red: 1.0-r, green: 1.0-g, blue: 1.0-b, alpha: a) : .black
     }
 }
